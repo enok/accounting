c5aff493e5ca80f4a0340c1845973953271b0b9b
Enok <enok.jesus@gmail.com>
2018-05-07 10:53:19 -0300

    SubGrupo: salvar - testes integrados (rest)

---
+++ accounting-rest/src/test/java/br/com/accounting/rest/controller/SubGrupoControllerTest.java
@@ -0,0 +1,144 @@
+package br.com.accounting.rest.controller;
+
+import br.com.accounting.business.dto.SubGrupoDTO;
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+public class SubGrupoControllerTest extends GenericTest {
+    @Autowired
+    private SubGrupoController controller;
+
+    private MockMvc mvc;
+    private Gson gson;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.standaloneSetup(controller).build();
+        gson = new Gson();
+    }
+
+    @Test
+    public void criarSemDiretorio() throws Exception {
+        deletarDiretorioEArquivos();
+
+        SubGrupoDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInsufficientStorage())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(507)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("Não foi possível buscar os registros.")));
+    }
+
+    @Test
+    public void criarSemNome() throws Exception {
+        SubGrupoDTO dto = getDTO()
+                .nome(null);
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(400)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("O campo nome é obrigatório.")));
+    }
+
+    @Test
+    public void criarSemNomeEDescricao() throws Exception {
+        SubGrupoDTO dto = getDTO()
+                .nome(null)
+                .descricao(null);
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(400)))
+                .andExpect(jsonPath("$.mensagens", hasSize(2)))
+                .andExpect(jsonPath("$.mensagens[0]", is("O campo nome é obrigatório.")))
+                .andExpect(jsonPath("$.mensagens[1]", is("O campo descrição é obrigatório.")));
+    }
+
+    @Test
+    public void criarDuplicado() throws Exception {
+        SubGrupoDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigos", hasSize(1)))
+                .andExpect(jsonPath("$.codigos[0]", is(0)));
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(400)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("SubGrupo duplicado.")));
+    }
+
+    @Test
+    public void criar() throws Exception {
+        SubGrupoDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/subgrupo")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigos", hasSize(1)))
+                .andExpect(jsonPath("$.codigos[0]", is(0)));
+    }
+
+    private SubGrupoDTO getDTO() {
+        return new SubGrupoDTO()
+                .nome("Suplementos")
+                .descricao("Sumplemntos comprados");
+    }
+}
