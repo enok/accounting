76372b2556c495988eaf8d5ab95295be52de4111
Enok <enok.jesus@gmail.com>
2018-05-07 10:50:37 -0300

    Local: salvar - testes integrados (rest)

---
+++ accounting-rest/src/test/java/br/com/accounting/rest/controller/LocalControllerTest.java
@@ -0,0 +1,123 @@
+package br.com.accounting.rest.controller;
+
+import br.com.accounting.business.dto.LocalDTO;
+import com.google.gson.Gson;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+public class LocalControllerTest extends GenericTest {
+    @Autowired
+    private LocalController controller;
+
+    private MockMvc mvc;
+    private Gson gson;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.standaloneSetup(controller).build();
+        gson = new Gson();
+    }
+
+    @Test
+    public void criarSemDiretorio() throws Exception {
+        deletarDiretorioEArquivos();
+
+        LocalDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/local")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isInsufficientStorage())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(507)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("Não foi possível buscar os registros.")));
+    }
+
+    @Test
+    public void criarSemNome() throws Exception {
+        LocalDTO dto = getDTO()
+                .nome(null);
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/local")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(400)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("O campo nome é obrigatório.")));
+    }
+
+    @Test
+    public void criarDuplicado() throws Exception {
+        LocalDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/local")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigos", hasSize(1)))
+                .andExpect(jsonPath("$.codigos[0]", is(0)));
+
+        mvc.perform(post("/local")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigo", is(400)))
+                .andExpect(jsonPath("$.mensagens", hasSize(1)))
+                .andExpect(jsonPath("$.mensagens[0]", is("Local duplicado.")));
+    }
+
+    @Test
+    public void criar() throws Exception {
+        LocalDTO dto = getDTO();
+        String json = gson.toJson(dto);
+
+        mvc.perform(post("/local")
+                .characterEncoding("UTF-8")
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.codigos", hasSize(1)))
+                .andExpect(jsonPath("$.codigos[0]", is(0)));
+    }
+
+    private LocalDTO getDTO() {
+        return new LocalDTO()
+                .nome("Site");
+    }
+}
