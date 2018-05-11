947af21abd9a64507b354309f46a285686fc97b0
Enok <enok.jesus@gmail.com>
2018-05-11 17:25:22 -0300

    RestControllerAdvice

--- accounting-rest/src/main/java/br/com/accounting/rest/controller/exception/AbstractExceptionHandler.java
+++ accounting-rest/src/main/java/br/com/accounting/rest/controller/exception/AbstractExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RestControllerAdvice
 public abstract class AbstractExceptionHandler {
 
     @ExceptionHandler(ValidationException.class)
